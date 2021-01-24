4cc05a42938b8c3fa07160e4768ab838bbc6b686
behuFTP <benjamin.dals.hughes@gmail.com>
2021-01-24 13:01:39 +0100

    test

--- src/server/api/routes/users.router.js
+++ src/server/api/routes/users.router.js
@@ -23,7 +23,7 @@ const usersController = require('../controllers/users.controller');
  *        description: Unexpected error.
  */
 router.get('/', (req, res, next) => {
-  console.log(result);
+  console.log('test');
   usersController
     .getUsers()
     .then((result) => res.json(result))
