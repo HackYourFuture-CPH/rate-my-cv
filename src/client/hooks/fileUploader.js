caee773693816a0b93e95ff69069ea07b73fd85f
hamed <h.karachi@gmail.com>
2021-01-24 01:52:45 +0100

    function to recive cv file and give url

--- src/client/hooks/fileUploader.js
+++ src/client/hooks/fileUploader.js
@@ -1,18 +1,15 @@
-import { storage } from './../firebase/configure';
+import { storage } from '../firebase/configure';
 
 export function FileUploader(cv) {
   cv.preventDefault();
 
   const uploadTask = storage.ref(`/CVs/${cv.name}`).put(cv);
-  //initiates the firebase side uploading
   uploadTask.on(
     'state_changed',
     (snapShot) => {
-      //takes a snap shot of the process as it is happening
       console.log(snapShot);
     },
     (err) => {
-      //catches the errors
       console.log(err);
     },
     () => {
