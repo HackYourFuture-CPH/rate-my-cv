0e519a9e4333be299771cb9dc2468f02a34e464e
shruthidasarapu <66563360+shruthidasarapu@users.noreply.github.com>
2021-01-26 14:58:29 +0100

    removed the spaces and added padding

--- src/client/components/SearchComponent/Search.js
+++ src/client/components/SearchComponent/Search.js
@@ -3,21 +3,21 @@ import search from '../../assets/images/SearchIcon.svg';
 import  './Search.styles.css';
 
 export default function Search() {
-    const [searchText, setSearchText] = useState(" ");
+    const [searchText, setSearchText] = useState("");
     const handleChange = (event)=> {
         setSearchText(event.target.value);
       };
   return (
     <div>
     <div className="search">
-     <img  className = "image" src= {search} alt= "search icon" />
+     <img className="image" src={search} alt="search icon" />
        <input className= "input-search"
         type="text"
         placeholder="Search by name, job title or date"
         onChange={handleChange}
        />
       </div>
-      <h4> {searchText}</h4>
+      <h4>{searchText}</h4>
     </div>
   );
 }
