0135a7a7db33c9ce9d8c3ef95b82b625737f342d
shruthidasarapu <66563360+shruthidasarapu@users.noreply.github.com>
2021-01-28 00:21:12 +0100

    Added style as a props

--- src/client/components/Button/Button.js
+++ src/client/components/Button/Button.js
@@ -2,12 +2,12 @@ import React from "react";
 import PropTypes from 'prop-types';
 import './Button.styles.css';
 
-const Button = ({ buttonName,backgroundColor}) => {
-  return <button className= "button" type="submit" style= {{backgroundColor}}> {buttonName} </button>;
+const Button = ({buttonName,style}) => {
+  return <button className= "button" type="button" style= {style}> {buttonName} </button>;
 };
 export default Button;
  
 Button.propTypes = {
   buttonName: PropTypes.string.isRequired,
-  backgroundColor:PropTypes.string.isRequired
+  style: PropTypes.string.isRequired
 };
