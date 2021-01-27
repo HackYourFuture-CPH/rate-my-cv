3c54c3d26e300e00fd645586c0d3e91314ca9ab1
Ember Blooming <ipoljupci@gmail.com>
2021-01-27 12:15:33 +0100

    updated firebase configuration

--- src/client/firebase/config.js
+++ src/client/firebase/config.js
@@ -1,9 +1,10 @@
 export default {
-  FIREBASE_APP_API_KEY: 'AIzaSyBzqc3kC3zuvZd_YQFQGTmeAV5QNFtZGPA',
-  FIREBASE_APP_AUTH_DOMAIN: 'rate-my-cv.firebaseapp.com',
-  FIREBASE_APP_DATABASE_URL: 'https://rate-my-cv-default-rtdb.firebaseio.com/',
-  FIREBASE_APP_PROJECT_ID: 'rate-my-cv',
-  FIREBASE_APP_STORAGE_BUCKET: 'rate-my-cv.appspot.com',
-  FIREBASE_APP_MESSAGING_SENDER_ID: '621675619680',
-  FIREBASE_APP_ID: '1:621675619680:web:9052d52891b84bb463ea05',
+  FIREBASE_APP_API_KEY: 'AIzaSyB1dAY0N5_b64w3Ssi9wsdPbuJhV6X5Cu4',
+  FIREBASE_APP_AUTH_DOMAIN: 'rate-my-cv-345eb.firebaseapp.com',
+  FIREBASE_APP_DATABASE_URL:
+    'https://rate-my-cv-345eb-default-rtdb.europe-west1.firebasedatabase.app/',
+  FIREBASE_APP_PROJECT_ID: 'rate-my-cv-345eb',
+  FIREBASE_APP_STORAGE_BUCKET: 'rate-my-cv-345eb.appspot.com',
+  FIREBASE_APP_MESSAGING_SENDER_ID: '625944750329',
+  FIREBASE_APP_ID: '1:625944750329:web:1c230059be833a09e45a28',
 };
