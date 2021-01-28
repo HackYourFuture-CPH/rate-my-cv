cdbc59272b937d5d9e50ff85440912aafdc363fa
unknown <ckmaina@gmail.com>
2021-01-28 16:44:13 +0100

    PageHeader to Upload CV, issue_41 done

---
+++ src/client/components/PageHeaderComponent/PageHeader.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Search from '../SearchComponent/Search';
+import Button from '../Button/Button';
+import './PageHeader.styles.css';
+
+export default function PageHeader() {
+  return (
+    <div>
+      <div className="cv-loader">
+        <div>Uploaded Resume</div>        
+          <div className = "cv-btn">
+            <Button buttonName={'Upload New Cv'} backgroundColor = {("backgroundColor", "black")} />
+          </div>
+        </div>
+      <div className="search-button">
+        <Search />
+      </div>
+    </div>
+  );
+}
