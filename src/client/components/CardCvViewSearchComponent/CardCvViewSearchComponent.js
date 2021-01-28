e686d829ecc25616b6d20eeea4e4b53b445957a5
Valentina Méndez <valentina.mdz@gmail.com>
2021-01-28 12:16:51 +0100

    Implement cardCv view-search component

---
+++ src/client/components/CardCvViewSearchComponent/CardCvViewSearchComponent.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import unknownUser from '../../assets/images/portraits/unknown-black.jpg';
+import './CardCvViewSearchComponent.styles.css';
+import { Button } from '../Button/Button';
+
+export const CardCvViewSearchComponent = ({
+  profileImageUrl,
+  fullName,
+  position,
+  fileUrl,
+  createdDate,
+}) => {
+  const parsedDate = (date) =>
+    new Date(date).toLocaleDateString('en-GB', { timeZone: 'UTC' });
+
+  /**
+   * Returns a name for a file using the first two elements
+   * in the name of a user without spaces
+   */
+  const setNameOfFile = (name, url) => {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    if (url) {
+      return `${name
+        .split(' ')
+        .slice(0, 2)
+        .reduce(reducer)}-CV.pdf`;
+    }
+    return 'no-file';
+  };
+
+  return (
+    <div className="card-cv-whole">
+      <div className="card-cv-top">
+        <div className="profile-picture">
+          <div className="circle-mask">
+            <img
+              src={profileImageUrl || unknownUser}
+              alt="Portrait of user"
+              className="user-photo"
+            />
+          </div>
+        </div>
+        <div className="user-info">
+          <p className="user-name">{fullName}</p>
+          <p className="user-position">{position}</p>
+        </div>
+      </div>
+      <div className="card-cv-middle">
+        <p className="file-url">{setNameOfFile(fullName, fileUrl)}</p>
+        <p className="created-date">{parsedDate(createdDate)}</p>
+      </div>
+      <div className="card-cv-bottom">
+        <a href={fileUrl} target="_blank" rel="noopener noreferrer">
+          <Button
+            buttonName={('Label', 'See resume')}
+            backgroundColor={('backgroundColor', '#0676F2')}
+          />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+CardCvViewSearchComponent.propTypes = {
+  profileImageUrl: PropTypes.string.isRequired,
+  fullName: PropTypes.string.isRequired,
+  position: PropTypes.string.isRequired,
+  fileUrl: PropTypes.string.isRequired,
+  createdDate: PropTypes.string.isRequired,
+};
---
+++ src/client/components/CardCvViewSearchComponent/CardCvViewSearchComponent.stories.js
@@ -0,0 +1,36 @@
+import man from '../../assets/images/portraits/man2.jpg';
+import woman from '../../assets/images/portraits/woman3.jpg';
+import { CardCvViewSearchComponent } from './CardCvViewSearchComponent';
+import React from 'react';
+
+export default { title: 'Card CV view-search component' };
+
+export const CardCVTest = () => (
+  <CardCvViewSearchComponent
+    profileImageUrl={man}
+    fullName="Jonathan Andersen"
+    position="Web developer"
+    fileUrl="https://zety.com/blog/banking-resume-example"
+    createdDate="Wed Jan 27 2021 12:42:41 GMT+0100 (Central European Standard Time)"
+  />
+);
+
+export const CardCVTestTwo = () => (
+  <CardCvViewSearchComponent
+    profileImageUrl=""
+    fullName="Ariana Valentina Méndez Jiménez"
+    position="Web developer"
+    fileUrl="https://zety.com/blog/banking-resume-example"
+    createdDate="Wed Jan 27 2021 12:42:41 GMT+0100 (Central European Standard Time)"
+  />
+);
+
+export const CardCVTestThree = () => (
+  <CardCvViewSearchComponent
+    profileImageUrl={woman}
+    fullName="Stephanie Mendrala"
+    position="Scientist"
+    fileUrl="https://zety.com/blog/banking-resume-example"
+    createdDate="Wed Jan 27 2021 12:42:41 GMT+0100 (Central European Standard Time)"
+  />
+);
