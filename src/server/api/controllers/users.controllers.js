ff4211b7d8c2415475f9921fe1298f654dad01f7
HYF <differ454@hotmail.com>
2021-01-30 00:23:52 +0100

    users.controllers.js changes implemented

--- src/server/api/controllers/users.controllers.js
+++ src/server/api/controllers/users.controllers.js
@@ -1,7 +1,6 @@
 /* TODO: This is an example controller to illustrate a server side controller.
 Can be deleted as soon as the first real controller is added. */
 const knex = require('../../config/db');
-const Error = require('../lib/utils/http-error');
 const moment = require('moment-timezone');
 
 
