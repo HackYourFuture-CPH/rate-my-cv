3589f68e76a3e2f4e22b3b2010db198b94b14578
HYF <differ454@hotmail.com>
2021-01-29 12:59:06 +0100

    footer implemented

--- src/client/App.js
+++ src/client/App.js
@@ -29,7 +29,7 @@ function App() {
         <AuthenticatedRoute
           exact
           path="/profile"
-          isAuthenticated={isAuthenticated}
+          isAuthenticated={isAuthenticated}  
         >
           <Profile />
         </AuthenticatedRoute>
--- src/client/components/Footer/Footer.stories.js
+++ src/client/components/Footer/Footer.stories.js
@@ -4,4 +4,4 @@ import  Footer  from './Footer';
 
 export default { title: 'Footer component' };
 
-export const footer = () => <Footer />;       
+export const footer = () => <Footer />;          
