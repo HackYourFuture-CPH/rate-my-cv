b3ad1d223018b67f4ab7f3e4d34c09bfafb32883
shruthidasarapu <66563360+shruthidasarapu@users.noreply.github.com>
2021-01-31 17:45:07 +0100

    added type and onclick as props

--- src/client/components/Button/Button.js
+++ src/client/components/Button/Button.js
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Button.styles.css';
 
-const Button = ({ buttonName, style }) => {
+const Button = ({ buttonName, style, type, onClick }) => {
+  const button = type;
   return (
-    <button className="button" type="button" style={style}>
+    <button className="button" type={button} style={style} onClick={onClick}>
       {buttonName}
     </button>
   );
@@ -14,6 +15,7 @@ export default Button;
 Button.propTypes = {
   buttonName: PropTypes.string.isRequired,
   style: PropTypes.string,
+  type: PropTypes.string.isRequired,
 };
 Button.defaultProps = {
   style: {},
