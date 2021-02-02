2aecf0c372dbe669105e9a41a5c0dd811984156a
arya56 <arya56@yahoo.com>
2021-02-02 19:33:42 +0100

    Header style on guest view

--- src/client/components/Navigation/Header.js
+++ src/client/components/Navigation/Header.js
@@ -1,46 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import UserIcon from '../../assets/images/icons/user-icon.png'
 // import { signOut } from '../../firebase/auth';
 
-export default function Header({ isAuthenticated = false, signOut }) {
-  const username = 'Ali Eh';
-  const [showSubMenu, setShowSubMenu] = useState(false);
-
-  if (isAuthenticated) { 
-    return (
-    <div className="header">
-    <div className="home-link">
-    <Link to="/" className="no-design">
-        <span className="review">Review</span><span className="resume">Resume</span>
-      </Link>
-    </div>
-    <ul className="right-section">
-        <li className="user-name">Ali Eheeee</li>
-        <li className="menu">
-            <form>
-                <input type="submit" className="icon" value="" style={{ backgroundImage: `url(${UserIcon})` }}/>
-              </form>
-              <div className="drop-down">
-                <div className='triangle'> </div>
-                <div className="nav-position_color">  
-                    <ul className="subnav">
-                  <li>
-                    <Link to="/profile" className="white-design"><span>Edit profile</span></Link>
-                  </li>
-                  <li>
-                    <Link to="/sign-in" className="white-design"><span>Sign out</span></Link>
-                  </li>
-                    </ul>
-                </div>
-                </div>
-        </li>
-    </ul>
-    </div>
-     );
-    }
+export default function Header({ isAuthenticated = false }) {
+ 
     return (
       <div className="header">
           <div className="home-link">
@@ -67,5 +33,4 @@ export default function Header({ isAuthenticated = false, signOut }) {
 
 Header.propTypes = {
   isAuthenticated: PropTypes.bool.isRequired,
-  signOut: PropTypes.func.isRequired,
 };
