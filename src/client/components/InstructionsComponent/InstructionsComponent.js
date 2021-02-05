c7b8aacfdaa65b023a9a7dcbd782eff09b344934
Kiran-Serolla <serollakiran@outlook.com>
2021-02-05 12:42:31 +0100

    implamented feedback

--- src/client/components/InstructionsComponent/InstructionsComponent.js
+++ src/client/components/InstructionsComponent/InstructionsComponent.js
@@ -1,33 +1,29 @@
-import React from 'react'
+import React from 'react';
 import upload from '../../assets/images/upload.png';
 import feedback from '../../assets/images/feedback.png';
 import notification from '../../assets/images/notification.png';
 import './InstructionsComponent.css';
 
-export const InstructionsComponent =() => {
-    return (
-        <div>
-            
-           <div className = "container">
-           <h2>How it Works</h2>
-           <div className = "all-images">
-           <div className = "image">
-           <img src={upload} alt="Uploadimage"/>
-           <p>Upload your resume</p>
-           </div>
-           <div className = "image">
-           <img src={feedback} alt="Feedbackimage"/>
-           <p>Others can take a look at your resume and give you feedback</p>
-           </div>
-           <div className = "image">
-           <img src={notification} alt="Notificationimage"/>
-           <p>You get a notification with feedback and rating</p>
-           </div>
-           </div>
-           
+export const InstructionsComponent = () => {
+  return (
+    <div>
+      <div className="container">
+        <h2>How it Works</h2>
+        <div className="all-images">
+          <div className="image">
+            <img src={upload} alt="Uploadimage" />
+            <p>Upload your resume</p>
+          </div>
+          <div className="image">
+            <img src={feedback} alt="Feedbackimage" />
+            <p>Others can take a look at your resume and give you feedback</p>
+          </div>
+          <div className="image">
+            <img src={notification} alt="Notificationimage" />
+            <p>You get a notification with feedback and rating</p>
+          </div>
         </div>
-           
-        </div>
-    )
-}
-
+      </div>
+    </div>
+  );
+};
