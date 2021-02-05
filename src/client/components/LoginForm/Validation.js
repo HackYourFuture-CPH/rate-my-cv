c6516c7ff2a5b79cd6750b020baa2d91c6b2e783
Kiran-Serolla <serollakiran@outlook.com>
2021-02-05 17:48:15 +0100

    Created Login Form

---
+++ src/client/components/LoginForm/Validation.js
@@ -0,0 +1,21 @@
+function Validation(values) {
+  let errors = {};
+  if (!values.email) {
+    errors.email = 'Email is required';
+  } else if (
+    !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+      values.email,
+    )
+  ) {
+    errors.email = 'Email address is invalid';
+  }
+
+  if (!values.password) {
+    errors.password = 'Password is required';
+  } else if (values.password.length < 6) {
+    errors.password = 'Password must be atleast 6 characters';
+  }
+  return errors;
+}
+
+export default Validation;
