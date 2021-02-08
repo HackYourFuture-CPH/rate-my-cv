398d170125d94a47d86db386e1c86508dd60ccc8
KavyaReddy732 <66561373+KavyaReddy732@users.noreply.github.com>
2021-02-08 14:45:55 +0100

    removed console.log statement

--- src/client/components/CvPdfRender.js
+++ src/client/components/CvPdfRender.js
@@ -4,14 +4,12 @@ import { useParams } from 'react-router-dom';
 
 export default function CvPdfRender() {
     const [cvPdfFile, setCvPdfFile] = useState()
-console.log(cvPdfFile)
     const {id} = useParams();
     useEffect (() => {
         (async() => { 
           const response = await fetch('/api/cv');
           const data = await response.json();
           const specificCv = data.find((cv)=>cv.fk_user_id === id) 
-                                console.log(specificCv)
             const src = `https://docs.google.com/viewer?url=${specificCv.file_url}&embedded=true`;
             setCvPdfFile(src)
         })()
