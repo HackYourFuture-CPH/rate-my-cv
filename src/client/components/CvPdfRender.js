313748088eca927a2e205126e868d482e610068c
KavyaReddy732 <66561373+KavyaReddy732@users.noreply.github.com>
2021-02-09 14:01:26 +0100

    formatted the code

--- src/client/components/CvPdfRender.js
+++ src/client/components/CvPdfRender.js
@@ -1,23 +1,23 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 
-export default function CvPdfRender() {
+export function CvPdfRender() {
     const [cvPdfFile, setCvPdfFile] = useState()
-    const {id} = useParams();
-    useEffect (() => {
-        (async() => { 
-          const response = await fetch('/api/cv');
-          const data = await response.json();
-          const specificCv = data.find((cv)=>cv.fk_user_id === id) 
+    const { id } = useParams();
+    useEffect(() => {
+        (async () => {
+            const response = await fetch('/api/cv');
+            const data = await response.json();
+            const specificCv = data.find((cv) => cv.fk_user_id === id)
             const src = `https://docs.google.com/viewer?url=${specificCv.file_url}&embedded=true`;
             setCvPdfFile(src)
         })()
-     },[id])
+    }, [id])
 
     return (
         <div>
-            <iframe title="CV" width="500" height="800" src={cvPdfFile} />
+            <iframe title="CV" frameBorder="0" style={{ width: "500px", height: "800px" }} src={cvPdfFile} />
         </div>
     )
 }
