e7876dd192aa6fda8af207bc1abf70979079514d
KavyaReddy732 <66561373+KavyaReddy732@users.noreply.github.com>
2021-02-10 12:44:27 +0100

    home page slogan developed

---
+++ src/client/components/Home/RegisterButton.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {useHistory} from 'react-router-dom';
+import Button from '../Button/Button';
+import './RegisterButton.style.css';
+
+function RegisterButton() {
+    let history = useHistory();
+
+    const redirect = () => {
+      history.push('/sign-up')
+    }
+    return (
+        <div className="homepage">
+          <section className="homepage-slogan">
+          <h2>Improve Your Resume To Present Your Best Self</h2>  
+          <p>Upload your Resume to get an evaluation</p>
+          <Button buttonName='Browse' style={{ backgroundColor: 'orange' }} onClick={redirect}/>
+          </section>
+        </div>
+    )
+}
+
+export default RegisterButton
---
+++ src/client/components/Home/RegisterButton.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import  RegisterButton  from './RegisterButton';
+
+export default { title: 'Home Page Slogan' };
+
+export  const Resume = ()=><div><RegisterButton /></div>
