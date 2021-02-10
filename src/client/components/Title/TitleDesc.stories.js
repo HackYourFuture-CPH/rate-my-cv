24e9a76812f64fd402d403846d681ce0394cedb7
Diego <differ454@hotmail.com>
2021-02-10 20:14:42 +0100

    0.2

--- src/client/components/Title/TitleDesc.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import  TitleDesc  from './TitleDesc';
-
-
-export default { title: 'Title component' };
-
-export const title = () => <TitleDesc title="Here goes the title" description="Heres goes the description" />;  
