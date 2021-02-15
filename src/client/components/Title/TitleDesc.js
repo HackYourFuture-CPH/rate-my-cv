91a4831e823b0aefdb3a6641a67992872d614f4b
Diego <differ454@hotmail.com>
2021-02-15 16:56:11 +0100

    changes implemented 0.1

--- src/client/components/Title/TitleDesc.js
+++ src/client/components/Title/TitleDesc.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './TitleDesc.styles.css';
-
+ 
 const TitleDesc = ({ title, description }) => {
   return (
     <div className="container">
