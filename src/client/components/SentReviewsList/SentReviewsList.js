3501e628c050a253fe9eb98cd5a93e8f05c613f5
Ember Blooming <ipoljupci@gmail.com>
2021-02-16 18:46:14 +0100

    made component skeleton

---
+++ src/client/components/SentReviewsList/SentReviewsList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './SentReviewsList.styles.css';
+
+export const SentReviewsList = ({}) => {
+  return (
+    <div className="sent-reviews-list">
+      <h2>Sent reviews</h2>
+    </div>
+  );
+};
+
+SentReviewsList.propTypes = {};
+
+SentReviewsList.defaultProps = {};
---
+++ src/client/components/SentReviewsList/SentReviewsList.stories.js
@@ -0,0 +1,6 @@
+import { SentReviewsList } from './SentReviewsList';
+import React from 'react';
+
+export default { title: 'Sent reviews list' };
+
+export const SentReviewsListOne = () => <SentReviewsList />;
