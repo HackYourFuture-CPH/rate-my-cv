44f26d5cca70b30144a1fa662c90f6cec2492a37
Ember Blooming <ipoljupci@gmail.com>
2021-02-17 08:18:57 +0100

    rendered a list of reviewed cvs, added data in stories

--- src/client/components/SentReviewsList/SentReviewsList.js
+++ src/client/components/SentReviewsList/SentReviewsList.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './SentReviewsList.styles.css';
+import { SentReviewsComponent } from '../SentReviewsComponent/SentReviewsComponent';
 
-export const SentReviewsList = ({}) => {
+export const SentReviewsList = ({ reviews }) => {
   return (
     <div className="sent-reviews-list">
       <h2>Sent reviews</h2>
+      {reviews.map((cvReviews) => {
+        return (
+          <div className="review-wrapper">
+            <SentReviewsComponent reviews={cvReviews} />
+          </div>
+        );
+      })}
     </div>
   );
 };
 
-SentReviewsList.propTypes = {};
+SentReviewsList.propTypes = {
+  reviews: PropTypes.arrayOf(
+    PropTypes.shape({
+      profileImageUrl: PropTypes.string,
+      fullName: PropTypes.string.isRequired,
+      createdDate: PropTypes.instanceOf(Date).isRequired,
+      review: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
 
 SentReviewsList.defaultProps = {};
--- src/client/components/SentReviewsList/SentReviewsList.stories.js
+++ src/client/components/SentReviewsList/SentReviewsList.stories.js
@@ -3,4 +3,90 @@ import React from 'react';
 
 export default { title: 'Sent reviews list' };
 
-export const SentReviewsListOne = () => <SentReviewsList />;
+export const SentReviewsListOne = () => (
+  <SentReviewsList
+    reviews={[
+      [
+        {
+          // Information for the person that gives the review
+          review:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          createdDate: new Date(),
+          reviewer: {
+            fullName: 'Ghofrane Ben Hmaid',
+            profileImageUrl: 'http://placekitten.com/200/200',
+          },
+          // Information for the person that receives the review
+          cv: {
+            fileUrl: 'http://www.africau.edu/images/default/sample.pdf',
+            averageStars: 4,
+            createdDate: new Date(),
+            author: {
+              fullName: 'Stephanie Hicks',
+              profileImageUrl: 'http://placekitten.com/150/150',
+            },
+          },
+        },
+      ],
+
+      [
+        {
+          review:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          createdDate: new Date(),
+          reviewer: {
+            fullName: 'Ghofrane Ben Hmaid',
+            profileImageUrl: 'http://placekitten.com/200/200',
+          },
+          cv: {
+            fileUrl: 'http://www.africau.edu/images/default/sample.pdf',
+            averageStars: 4,
+            createdDate: new Date(),
+            author: {
+              fullName: 'Stephanie Hicks',
+              profileImageUrl: 'http://placekitten.com/150/150',
+            },
+          },
+        },
+
+        {
+          review:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          createdDate: new Date(),
+          reviewer: {
+            fullName: 'Ghofrane Ben Hmaid',
+            profileImageUrl: 'http://placekitten.com/200/200',
+          },
+          cv: {
+            fileUrl: 'http://www.africau.edu/images/default/sample.pdf',
+            averageStars: 4,
+            createdDate: new Date(),
+            author: {
+              fullName: 'Stephanie Mendrala',
+              profileImageUrl: 'http://placekitten.com/250/250',
+            },
+          },
+        },
+
+        {
+          review:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          createdDate: new Date(),
+          reviewer: {
+            fullName: 'Ghofrane Ben Hmaid',
+            profileImageUrl: 'http://placekitten.com/200/200',
+          },
+          cv: {
+            fileUrl: 'http://www.africau.edu/images/default/sample.pdf',
+            averageStars: 4,
+            createdDate: new Date(),
+            author: {
+              fullName: 'Valentina Méndez',
+              profileImageUrl: '',
+            },
+          },
+        },
+      ],
+    ]}
+  />
+);
