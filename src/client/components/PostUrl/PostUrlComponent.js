8f5bc65526ac3ec20980a07d2fef5b2950c2f7fa
arya56 <arya56@yahoo.com>
2021-02-22 14:56:59 +0100

    getting user data from Auth file

--- src/client/components/PostUrl/PostUrlComponent.js
+++ src/client/components/PostUrl/PostUrlComponent.js
@@ -2,65 +2,42 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import Loader from '../Loader';
+import { useAuthentication } from '../../hooks/useAuthentication';
 
-export default function PostUrlComponent({
-  uploadedFile,
-  title,
-  description,
-  firebaseToken,
-}) {
+export default function PostUrlComponent({ uploadedFile, title, description }) {
+  const { userData } = useAuthentication();
   const history = useHistory();
   const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-  const [fetchUserId, setFetchUserId] = useState(null);
   useEffect(() => {
-    setIsLoading(true);
-    (async () => {
-      try {
-        const response = await fetch('/api/users');
-        if (response.status !== 200) {
-          throw new Error('fail to connect to the Api');
+    if (userData) {
+      setIsLoading(true);
+      (async () => {
+        try {
+          const postCv = await fetch('/api/cvs', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              title,
+              description,
+              file_url: uploadedFile,
+              fk_user_id: userData.id,
+            }),
+          });
+          setIsLoading(false);
+          if (!postCv.ok) {
+            throw new Error(postCv.statusText);
+            // directing to the #165 component (Hasn't merge yet)
+          } else {
+            history.push('/profile');
+          }
+        } catch (error) {
+          throw new Error(error);
         }
-        const users = await response.json();
-        setIsLoading(false);
-        const currentUser = users.find(
-          (user) => user.firebase_token === firebaseToken,
-        );
-        setFetchUserId(currentUser.id);
-      } catch (error) {
-        setErrorMessage(error.message);
-      }
-    })();
-  }, [uploadedFile, title, description, firebaseToken]);
-  useEffect(() => {
-    setIsLoading(true);
-    (async () => {
-      try {
-        const postCv = await fetch('/api/cvs', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            title,
-            description,
-            file_url: uploadedFile,
-            fk_user_id: fetchUserId,
-          }),
-        });
-        setIsLoading(false);
-        if (!postCv.ok) {
-          // error would be sent conresponding component story #165
-          setErrorMessage(postCv.statusText);
-          throw new Error(postCv.statusText);
-        } else {
-          history.push('/sign-in');
-        }
-      } catch (error) {
-        throw new Error(error);
-      }
-    })();
-  }, [fetchUserId]);
+      })();
+    }
+  }, [description, history, title, uploadedFile, userData]);
 
   return isLoading ? <Loader /> : '';
 }
@@ -69,5 +46,4 @@ PostUrlComponent.propTypes = {
   uploadedFile: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  firebaseToken: PropTypes.string.isRequired,
 };
