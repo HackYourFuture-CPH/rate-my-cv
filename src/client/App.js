ac6f68b8a7ce2884842d6f2b620fd7ed56d12686
Diego <differ454@hotmail.com>
2021-02-23 10:48:09 +0100

    footer tag changed place serach filter component tag deleted from App.js

--- src/client/App.js
+++ src/client/App.js
@@ -6,11 +6,9 @@ import SignUp from './containers/SignUp';
 import ResetPassword from './containers/ResetPassword';
 import AuthenticatedRoute from './components/Auth/AuthenticatedRoute';
 import { useAuthentication } from './hooks/useAuthentication';
-import Header from './components/Navigation/Header';
 import Profile from './containers/Profile';
 import Loader from './components/Loader';
 import NotFoundPage from './containers/NotFound/notFoundPage.component';
-import SearchFilter from './components/SearchFilterComponent/SearchFilter.js';
 import Footer from './components/Footer/Footer';
 
 function App() {
@@ -20,7 +18,7 @@ function App() {
     <Router>
       {location.pathname === '/sign-in' ||
       location.pathname === '/sign-up' ? null : (
-        <Header isAuthenticated={isAuthenticated} username="William Henry Gates" />
+        <Footer />
       )}
       <Switch>
         <Route exact path="/">
@@ -38,8 +36,6 @@ function App() {
         </AuthenticatedRoute>
         <Route path="*" component={NotFoundPage} />
       </Switch>
-      <SearchFilter/>
-      <Footer/>
     </Router>
   );
 }
