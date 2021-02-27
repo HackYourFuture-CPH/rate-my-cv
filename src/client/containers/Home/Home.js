a136a0f905959cf385b5bba9fd1f1b7ff0973f1c
shruthidasarapu <66563360+shruthidasarapu@users.noreply.github.com>
2021-02-27 17:33:09 +0100

    try to fix the build error

--- src/client/containers/Home/Home.js
+++ src/client/containers/Home/Home.js
@@ -19,6 +19,7 @@ export const Home = () => {
       setCvs(result);
     })();
   }, []);
+
   const history = useHistory();
   const allResumes = () => {
     return <Router history={history.push('/dashboard')} />;
