e6527fbeb2a8904ef05eacb99fc759a90c96332f
KavyaReddy732 <66561373+KavyaReddy732@users.noreply.github.com>
2021-02-28 17:04:58 +0100

    static code error

--- src/client/components/AddResume/AddResume.js
+++ src/client/components/AddResume/AddResume.js
@@ -46,7 +46,7 @@ export const AddResume = ({isShown,setIsShown}) => {
   return ReactDom.createPortal(isShown ? (
     <div className="popup">
       <div className="popup-form">
-        <img className="close-icon" src={close} alt="close Icon" onClick={closeModel}/>
+        <img className="close-icon" src={close} alt="close Icon" onClick={closeModel} onKeyDown={closeModel}/>
         <h3 className="text">Upload new CV</h3>
         <div className="uploadform">
           <div className="title">
